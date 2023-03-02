e771fb14afc1216696227b150f369a48ee6358e4
Newton Ombese <newtonombese1@gmail.com>
2023-03-02 18:55:39 +0300

    :art:feat: Working on the hero section adding the image and text flexed on the right and left

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,110 @@
 import React from 'react'
 import styled from 'styled-components'
+import Navbar from './Navbar'
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const Title = styled.h1`
+  font-size: 74px;
+`;
+
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Line = styled.img`
+  height: 5px;
+`;
+
+const Subtitle = styled.h2`
+  color: #da4ea2;
+`;
+
+const Description = styled.p`
+  font-size: 24px;
+  color: lightgray;
+`;
+
+const Button = styled.button`
+  width: 100px;
+  padding: 10px;
+  background-color: #da4ea2;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-weight: 500;
+`;
+
+const Right = styled.div`
+  flex: 3;
+  position: relative;
+`;
+
+const Img = styled.img`
+  width: 800px;
+  height: 600px;
+  object-fit: contain;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  animation: animate 2s infinite ease alternate;
+  
+  @keyframes animate {
+    to{
+      transform: translateY(20px);
+    }
+  }
+`;
 
 const Hero = () => {
   return (
-    <Section>Hero</Section>
+    <Section>
+      <Navbar />
+      <Container>
+        <Left>
+          <Title>Think. Make. Solve.</Title>
+          <WhatWeDo>
+            <Line src="./img/line.png" />
+            <Subtitle>What We DO</Subtitle>
+          </WhatWeDo>
+          <Description>We enjoy creating delightful, human-centered digital experiences.</Description>
+          <Button>Learn More</Button>
+        </Left>
+        <Right>
+          {/* 3d model */}
+          <Img src="./img/moon.png" />
+        </Right>
+      </Container>
+    </Section>
   )
 }
 
