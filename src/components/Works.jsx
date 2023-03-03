0eca16676e214ad4926f7fa01fc4223e7851407f
Newton Ombese <newtonombese1@gmail.com>
2023-03-03 20:01:21 +0300

    :art:feat: Working on the works section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,14 +1,61 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const data = [
+  "Web Design",
+  "Development",
+  "Illustration",
+  "Product Design",
+  "Social Media",
+]
+
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`
+const Container = styled.div`
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`
+const List  = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`
+const ListItem = styled.li`
+  font-size: 100px;
+  font-weight: bold;
+  cursor: pointer;
+  color: transparent;
+  -webkit-text-stroke: 1px white;
+`
+const Right = styled.div`
+  flex: 1;
 `
 
 const Works = () => {
   return (
-    <Section>Works</Section>
+    <Section>
+      <Container>
+        <Left>
+          <List>
+            {data.map((item)=>(
+              <ListItem key={item}>{item}</ListItem>
+            ))}
+          </List>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
   )
 }
 
