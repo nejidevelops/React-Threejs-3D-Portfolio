67a271123ffa9ade2ccbae03bb9cfe636d36fa76
Newton Ombese <newtonombese1@gmail.com>
2023-03-03 20:24:38 +0300

    :lipstick:art:style: Adding animated to the data added

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -32,11 +32,35 @@ const List  = styled.ul`
   gap: 20px;
 `
 const ListItem = styled.li`
-  font-size: 100px;
+  font-size: 90px;
   font-weight: bold;
   cursor: pointer;
   color: transparent;
   -webkit-text-stroke: 1px white;
+  position: relative;
+
+  ::after {
+    content: '${(props)=>props.text}';
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: pink;
+    width: 0;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+  &:hover {
+    ::after {
+      animation: moveText 0.5s linear both;
+
+      @keyframes moveText {
+        to{
+          width: 100%;
+        }
+      }
+    }
+  }
 `
 const Right = styled.div`
   flex: 1;
@@ -49,7 +73,7 @@ const Works = () => {
         <Left>
           <List>
             {data.map((item)=>(
-              <ListItem key={item}>{item}</ListItem>
+              <ListItem key={item} text={item}>{item}</ListItem>
             ))}
           </List>
         </Left>
