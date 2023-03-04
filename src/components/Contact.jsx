2fa4f6a23c3fb9d02329cf11c27af15b733e41b7
Newton Ombese <newtonombese1@gmail.com>
2023-03-04 08:37:32 +0300

    :sparkles::art::tada:feat: Successful and functional form incoorporated with three.js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,74 +1,125 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import styled from "styled-components";
+// import Map from "./Map";
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
-`
+`;
+
 const Container = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
   justify-content: space-between;
   gap: 50px;
-  height: 100%;
-  width: 100%;
-`
+`;
+
 const Left = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
-  justify-content: center;
-`
+  justify-content: flex-end;
+  @media only screen and (max-width: 768px) {
+    justify-content: center;
+  }
+`;
+
 const Title = styled.h1`
   font-weight: 200;
-`
+`;
+
 const Form = styled.form`
-  width: 580px;
+  width: 500px;
   display: flex;
   flex-direction: column;
   gap: 25px;
+  @media only screen and (max-width: 768px) {
+    width: 300px;
+  }
+`;
 
-`
 const Input = styled.input`
   padding: 20px;
   background-color: #e8e6e6;
   border: none;
   border-radius: 5px;
-`
+`;
+
 const TextArea = styled.textarea`
   padding: 20px;
-  background-color: #e8e6e6;
   border: none;
   border-radius: 5px;
-`
+  background-color: #e8e6e6;
+`;
+
 const Button = styled.button`
   background-color: #da4ea2;
-   color: white;
-   border: none;
-   font-weight: bold;
-   cursor: pointer;
-   border-radius: 5px;
-   padding: 20px;
-`
+  color: white;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 5px;
+  padding: 20px;
+`;
+
 const Right = styled.div`
   flex: 1;
-`
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
 const Contact = () => {
+  const ref = useRef();
+  const [success, setSuccess] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_qhebjnj',
+        'template_7p1cmae',
+        ref.current,
+        'BvzRxuAYjXLhBMZWv'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setSuccess(true);
+        },
+        (error) => {
+          console.log(error.text);
+          setSuccess(false);
+        }
+      );
+  };
   return (
     <Section>
       <Container>
         <Left>
-          <Form>
+          <Form ref={ref} onSubmit={handleSubmit}>
             <Title>Contact Us</Title>
-            <Input placeholder='Enter your Name' />
-            <Input placeholder='Enter your Email' />
-            <TextArea placeholder='Write your message' />
-            <Button>Send</Button>
+            <Input placeholder="Name" name="name" />
+            <Input placeholder="Email" name="email" />
+            <TextArea
+              placeholder="Write your message"
+              name="message"
+              rows={10}
+            />
+            <Button type="submit">Send</Button>
+            {success &&
+              "Your message has been sent. We'll get back to you soon :)"}
           </Form>
         </Left>
-        <Right></Right>
+        <Right>
+          {/* <Map /> */}
+        </Right>
       </Container>
     </Section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
